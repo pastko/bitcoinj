cef59581e8280eef3c4ad830f56d3b7e3fa4ac23
hyun sik <storyofstory2@gmail.com>
2023-06-08 15:12:13 +0900

    change : bitcoinCashj NetworkParameters

--- core/src/main/java/org/bitcoinCashj/params/AbstractBitcoinNetParams.java
+++ core/src/main/java/org/bitcoinCashj/params/AbstractBitcoinNetParams.java
@@ -1,202 +1,171 @@
-/*
- * Copyright 2013 Google Inc.
- * Copyright 2015 Andreas Schildbach
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
 package org.bitcoinCashj.params;
 
-import static com.google.common.base.Preconditions.checkState;
-
+import com.google.common.base.Preconditions;
 import java.math.BigInteger;
-import java.util.concurrent.TimeUnit;
-
+import org.bitcoinCashj.core.BitcoinSerializer;
 import org.bitcoinCashj.core.Block;
+import org.bitcoinCashj.core.BlockChain;
 import org.bitcoinCashj.core.Coin;
 import org.bitcoinCashj.core.NetworkParameters;
-import org.bitcoinCashj.core.Sha256Hash;
 import org.bitcoinCashj.core.StoredBlock;
 import org.bitcoinCashj.core.Transaction;
-import org.bitcoinCashj.core.TransactionOutput;
 import org.bitcoinCashj.core.Utils;
-import org.bitcoinCashj.utils.MonetaryFormat;
-import org.bitcoinCashj.core.VerificationException;
 import org.bitcoinCashj.store.BlockStore;
 import org.bitcoinCashj.store.BlockStoreException;
+import org.bitcoinCashj.utils.MonetaryFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Stopwatch;
-
-import org.bitcoinCashj.core.BitcoinSerializer;
-
-/**
- * Parameters for Bitcoin-like networks.
- */
 public abstract class AbstractBitcoinNetParams extends NetworkParameters {
-
-    /**
-     * Scheme part for Bitcoin URIs.
-     */
-    public static final String BITCOIN_SCHEME = "bitcoin";
+    public static final String BITCOIN_SCHEME = "bitcoincash";
     public static final int REWARD_HALVING_INTERVAL = 210000;
-
-    private static final Logger log = LoggerFactory.getLogger(AbstractBitcoinNetParams.class);
-
-    /** lazy-initialized by the first call to {@link NetworkParameters#getGenesisBlock()} */
-    protected Block genesisBlock;
+    public static final int MAX_BITS = 486604799;
+    public static final String MAX_BITS_STRING = "1d00ffff";
+    public static final BigInteger MAX_TARGET = Utils.decodeCompactBits(486604799L);
+    private static BigInteger LARGEST_HASH;
+    private static final Logger log;
 
     public AbstractBitcoinNetParams() {
-        super();
-        interval = INTERVAL;
-        subsidyDecreaseBlockCount = REWARD_HALVING_INTERVAL;
+        this.interval = 2016;
+        this.subsidyDecreaseBlockCount = 210000;
     }
 
-    /**
-     * Checks if we are at a reward halving point.
-     * @param previousHeight The height of the previous stored block
-     * @return If this is a reward halving point
-     */
-    public final boolean isRewardHalvingPoint(final int previousHeight) {
-        return ((previousHeight + 1) % REWARD_HALVING_INTERVAL) == 0;
+    public final boolean isRewardHalvingPoint(int previousHeight) {
+        return (previousHeight + 1) % 210000 == 0;
     }
 
-    /**
-     * <p>A utility method that calculates how much new Bitcoin would be created by the block at the given height.
-     * The inflation of Bitcoin is predictable and drops roughly every 4 years (210,000 blocks). At the dawn of
-     * the system it was 50 coins per block, in late 2012 it went to 25 coins per block, and so on. The size of
-     * a coinbase transaction is inflation plus fees.</p>
-     *
-     * <p>The half-life is controlled by {@link NetworkParameters#getSubsidyDecreaseBlockCount()}.</p>
-     *
-     * @param height the height of the block to calculate inflation for
-     */
     public Coin getBlockInflation(int height) {
-        return Coin.FIFTY_COINS.shiftRight(height / getSubsidyDecreaseBlockCount());
+        return Coin.FIFTY_COINS.shiftRight(height / this.getSubsidyDecreaseBlockCount());
     }
 
-    /**
-     * Checks if we are at a difficulty transition point.
-     * @param previousHeight The height of the previous stored block
-     * @return If this is a difficulty transition point
-     */
-    public final boolean isDifficultyTransitionPoint(final int previousHeight) {
-        return ((previousHeight + 1) % this.getInterval()) == 0;
+    public static boolean isDifficultyTransitionPoint(StoredBlock storedPrev, NetworkParameters parameters) {
+        return (storedPrev.getHeight() + 1) % parameters.getInterval() == 0;
     }
 
-    @Override
-    public void checkDifficultyTransitions(final StoredBlock storedPrev, final Block nextBlock,
-        final BlockStore blockStore) throws VerificationException, BlockStoreException {
-        final Block prev = storedPrev.getHeader();
+    public static boolean isDifficultyTransitionPoint(int height, NetworkParameters parameters) {
+        return (height + 1) % parameters.getInterval() == 0;
+    }
 
-        // Is this supposed to be a difficulty transition point?
-        if (!isDifficultyTransitionPoint(storedPrev.getHeight())) {
-
-            // No ... so check the difficulty didn't actually change.
-            if (nextBlock.getDifficultyTarget() != prev.getDifficultyTarget())
-                throw new VerificationException("Unexpected change in difficulty at height " + storedPrev.getHeight() +
-                        ": " + Long.toHexString(nextBlock.getDifficultyTarget()) + " vs " +
-                        Long.toHexString(prev.getDifficultyTarget()));
-            return;
-        }
-
-        // We need to find a block far back in the chain. It's OK that this is expensive because it only occurs every
-        // two weeks after the initial block chain download.
-        final Stopwatch watch = Stopwatch.createStarted();
-        Sha256Hash hash = prev.getHash();
-        StoredBlock cursor = null;
-        final int interval = this.getInterval();
-        for (int i = 0; i < interval; i++) {
-            cursor = blockStore.get(hash);
-            if (cursor == null) {
-                // This should never happen. If it does, it means we are following an incorrect or busted chain.
-                throw new VerificationException(
-                        "Difficulty transition point but we did not find a way back to the last transition point. Not found: " + hash);
+    public static boolean isMonolithEnabled(StoredBlock storedPrev, BlockStore store, NetworkParameters parameters) {
+        if (storedPrev.getHeight() < 524626) {
+            return false;
+        } else {
+            try {
+                long mtp = BlockChain.getMedianTimestampOfRecentBlocks(storedPrev, store);
+                return isMonolithEnabled(mtp, parameters);
+            } catch (BlockStoreException var5) {
+                throw new RuntimeException("Cannot determine monolith activation without BlockStore");
             }
-            hash = cursor.getHeader().getPrevBlockHash();
         }
-        checkState(cursor != null && isDifficultyTransitionPoint(cursor.getHeight() - 1),
-                "Didn't arrive at a transition point.");
-        watch.stop();
-        if (watch.elapsed(TimeUnit.MILLISECONDS) > 50)
-            log.info("Difficulty transition traversal took {}", watch);
-
-        Block blockIntervalAgo = cursor.getHeader();
-        int timespan = (int) (prev.getTimeSeconds() - blockIntervalAgo.getTimeSeconds());
-        // Limit the adjustment step.
-        final int targetTimespan = this.getTargetTimespan();
-        if (timespan < targetTimespan / 4)
-            timespan = targetTimespan / 4;
-        if (timespan > targetTimespan * 4)
-            timespan = targetTimespan * 4;
-
-        BigInteger newTarget = Utils.decodeCompactBits(prev.getDifficultyTarget());
-        newTarget = newTarget.multiply(BigInteger.valueOf(timespan));
-        newTarget = newTarget.divide(BigInteger.valueOf(targetTimespan));
-
-        if (newTarget.compareTo(this.getMaxTarget()) > 0) {
-            log.info("Difficulty hit proof of work limit: {}", newTarget.toString(16));
-            newTarget = this.getMaxTarget();
-        }
-
-        int accuracyBytes = (int) (nextBlock.getDifficultyTarget() >>> 24) - 3;
-        long receivedTargetCompact = nextBlock.getDifficultyTarget();
-
-        // The calculated difficulty is to a higher precision than received, so reduce here.
-        BigInteger mask = BigInteger.valueOf(0xFFFFFFL).shiftLeft(accuracyBytes * 8);
-        newTarget = newTarget.and(mask);
-        long newTargetCompact = Utils.encodeCompactBits(newTarget);
-
-        if (newTargetCompact != receivedTargetCompact)
-            throw new VerificationException("Network provided difficulty bits do not match what was calculated: " +
-                    Long.toHexString(newTargetCompact) + " vs " + Long.toHexString(receivedTargetCompact));
     }
 
-    @Override
+    public static BigInteger ComputeTarget(StoredBlock pindexFirst, StoredBlock pindexLast) {
+        Preconditions.checkState(pindexLast.getHeight() > pindexFirst.getHeight());
+        BigInteger work = pindexLast.getChainWork().subtract(pindexFirst.getChainWork());
+        work = work.multiply(BigInteger.valueOf(600L));
+        long nActualTimespan = pindexLast.getHeader().getTimeSeconds() - pindexFirst.getHeader().getTimeSeconds();
+        if (nActualTimespan > 172800L) {
+            nActualTimespan = 172800L;
+        } else if (nActualTimespan < 43200L) {
+            nActualTimespan = 43200L;
+        }
+
+        work = work.divide(BigInteger.valueOf(nActualTimespan));
+        return LARGEST_HASH.divide(work).subtract(BigInteger.ONE);
+    }
+
+    public static BigInteger computeAsertTarget(NetworkParameters networkParameters, BigInteger refTarget, BigInteger referenceBlockAncestorTime, BigInteger referenceBlockHeight, BigInteger evalBlockTime, BigInteger evalBlockHeight, StoredBlock storedPrev, Block nextBlock) {
+        Preconditions.checkState(evalBlockHeight.compareTo(referenceBlockHeight) >= 0);
+        if (storedPrev != null && nextBlock != null && networkParameters.allowMinDifficultyBlocks() && nextBlock.getTimeSeconds() > storedPrev.getHeader().getTimeSeconds() + 1200L) {
+            return new BigInteger("1d00ffff", 16);
+        } else {
+            BigInteger heightDiff = evalBlockHeight.subtract(referenceBlockHeight);
+            BigInteger timeDiff = evalBlockTime.subtract(referenceBlockAncestorTime);
+            BigInteger halfLife = BigInteger.valueOf(networkParameters.getAsertHalfLife());
+            BigInteger rbits = BigInteger.valueOf(16L);
+            BigInteger radix = BigInteger.ONE.shiftLeft(rbits.intValue());
+            BigInteger heightDiffWithOffset = heightDiff.add(BigInteger.ONE);
+            BigInteger targetHeightOffsetMultiple = TARGET_SPACING_BIGINT.multiply(heightDiffWithOffset);
+            BigInteger exponent = timeDiff.subtract(targetHeightOffsetMultiple);
+            exponent = exponent.shiftLeft(rbits.intValue());
+            exponent = exponent.divide(halfLife);
+            BigInteger numShifts = exponent.shiftRight(rbits.intValue());
+            exponent = exponent.subtract(numShifts.shiftLeft(rbits.intValue()));
+            BigInteger factor = BigInteger.valueOf(195766423245049L).multiply(exponent);
+            factor = factor.add(BigInteger.valueOf(971821376L).multiply(exponent.pow(2)));
+            factor = factor.add(BigInteger.valueOf(5127L).multiply(exponent.pow(3)));
+            factor = factor.add(BigInteger.valueOf(2L).pow(47));
+            factor = factor.shiftRight(48);
+            BigInteger target = refTarget.multiply(radix.add(factor));
+            if (numShifts.compareTo(BigInteger.ZERO) < 0) {
+                target = target.shiftRight(-numShifts.intValue());
+            } else {
+                target = target.shiftLeft(numShifts.intValue());
+            }
+
+            target = target.shiftRight(16);
+            if (target.equals(BigInteger.ZERO)) {
+                return BigInteger.valueOf(Utils.encodeCompactBits(BigInteger.ONE));
+            } else {
+                return target.compareTo(MAX_TARGET) > 0 ? new BigInteger("1d00ffff", 16) : BigInteger.valueOf(Utils.encodeCompactBits(target));
+            }
+        }
+    }
+
+    public static BigInteger computeAsertTarget(NetworkParameters networkParameters, BigInteger refTarget, BigInteger referenceBlockAncestorTime, BigInteger referenceBlockHeight, BigInteger evalBlockTime, BigInteger evalBlockHeight) {
+        return computeAsertTarget(networkParameters, refTarget, referenceBlockAncestorTime, referenceBlockHeight, evalBlockTime, evalBlockHeight, (StoredBlock)null, (Block)null);
+    }
+
+    public static BigInteger computeAsertTarget(NetworkParameters networkParameters, int referenceBlockBits, BigInteger referenceBlockAncestorTime, BigInteger referenceBlockHeight, BigInteger evalBlockTime, BigInteger evalBlockHeight) {
+        BigInteger refTarget = Utils.decodeCompactBits((long)referenceBlockBits);
+        return computeAsertTarget(networkParameters, refTarget, referenceBlockAncestorTime, referenceBlockHeight, evalBlockTime, evalBlockHeight);
+    }
+
+    public static boolean isMonolithEnabled(long medianTimePast, NetworkParameters parameters) {
+        return medianTimePast >= parameters.getMonolithActivationTime();
+    }
+
+    public static boolean isAsertEnabled(StoredBlock storedPrev, BlockStore blockStore, NetworkParameters parameters) {
+        try {
+            long mtp = BlockChain.getMedianTimestampOfRecentBlocks(storedPrev, blockStore);
+            return mtp >= parameters.getAsertUpdateTime();
+        } catch (BlockStoreException var5) {
+            var5.printStackTrace();
+            return false;
+        }
+    }
+
     public Coin getMaxMoney() {
         return MAX_MONEY;
     }
 
-    /** @deprecated use {@link TransactionOutput#getMinNonDustValue()} */
-    @Override
-    @Deprecated
     public Coin getMinNonDustOutput() {
         return Transaction.MIN_NONDUST_OUTPUT;
     }
 
-    @Override
     public MonetaryFormat getMonetaryFormat() {
         return new MonetaryFormat();
     }
 
-    @Override
-    public int getProtocolVersionNum(final ProtocolVersion version) {
+    public int getProtocolVersionNum(NetworkParameters.ProtocolVersion version) {
         return version.getBitcoinProtocolVersion();
     }
 
-    @Override
     public BitcoinSerializer getSerializer(boolean parseRetain) {
         return new BitcoinSerializer(this, parseRetain);
     }
 
-    @Override
-    public String getUriScheme() {
-        return BITCOIN_SCHEME;
-    }
-
-    @Override
     public boolean hasMaxMoney() {
         return true;
+    }
+
+    static {
+        LARGEST_HASH = BigInteger.ONE.shiftLeft(256);
+        log = LoggerFactory.getLogger(AbstractBitcoinNetParams.class);
     }
 }
